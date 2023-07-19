a913a340515a922698b1e49067d57ae6f0b4f00e
Surbhi <surbhikumaridav@gmail.com>
2023-07-19 12:37:28 +0530

    added crud ui

--- lib/hidden.dart
+++ lib/hidden.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hidden_local_storage/storage_data_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HiddenPage extends StatefulWidget {
@@ -11,16 +12,27 @@ class HiddenPage extends StatefulWidget {
 }
 
 class _HiddenPageState extends State<HiddenPage> {
+  final List<StorageData> _storageData = [];
   List<Map<String, dynamic>> switchItems = [
     {"name": "staging", "value": false},
     {"name": "Production", "value": false},
     {"name": "debug", "value": false},
   ];
 
+  final TextEditingController _keyController = TextEditingController();
+  final TextEditingController _valueController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     loadSwitchValues();
+  }
+
+  void _addNewStorageData(String key, dynamic value) {
+    final newSt = StorageData(key: key, value: value);
+    setState(() {
+      _storageData.add(newSt);
+    });
   }
 
   void loadSwitchValues() async {
@@ -39,6 +51,52 @@ class _HiddenPageState extends State<HiddenPage> {
   void saveSwitchValue(String name, bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool(name, value);
+  }
+
+  Widget _buildStorageDataRow(StorageData storageData) {
+    return Container(
+      padding: EdgeInsets.only(left: 2, top: 4, bottom: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Key',
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                ),
+              ),
+              controller: _keyController,
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Expanded(
+            flex: 1,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Value',
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                ),
+              ),
+              controller: _valueController,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _storageData.remove(storageData);
+              });
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -62,42 +120,56 @@ class _HiddenPageState extends State<HiddenPage> {
               },
             );
           }).toList(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                // optional flex property if flex is 1 because the default flex is 1
-                flex: 1,
-                child: TextField(
-                  decoration: InputDecoration(
+          ..._storageData.map((data) => _buildStorageDataRow(data)).toList(),
+          Container(
+            padding: EdgeInsets.only(left: 2, top: 4, bottom: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    decoration: InputDecoration(
                       labelText: 'Key',
                       border: OutlineInputBorder(),
                       labelStyle: TextStyle(
-                          color: Colors.grey[400]
-                      )
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    controller: _keyController,
                   ),
                 ),
-              ),
-              SizedBox(width: 10.0),
-              Expanded(
-                // optional flex property if flex is 1 because the default flex is 1
-                flex: 1,
-                child: TextField(
-                  decoration: InputDecoration(
+                SizedBox(width: 10.0),
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    decoration: InputDecoration(
                       labelText: 'Value',
                       border: OutlineInputBorder(),
                       labelStyle: TextStyle(
-                          color: Colors.grey[400]
-                      )
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    controller: _valueController,
                   ),
                 ),
-              ),
-            ],
+                IconButton(
+                  onPressed: () {
+                    final key = _keyController.text;
+                    final value = _valueController.text;
+
+                    _addNewStorageData(key, value);
+
+                    _keyController.clear();
+                    _valueController.clear();
+                  },
+                  icon: Icon(Icons.add),
+                ),
+              ],
+            ),
           ),
-          ElevatedButton(onPressed: (){}, child: Text("ADD"))
         ],
       ),
     );
   }
 }
-
---
+++ lib/storage_data_model.dart
@@ -0,0 +1,8 @@
+class StorageData {
+ final String key;
+ final dynamic value;
+
+ StorageData({
+   required this.key,required this.value,
+ });
+}
